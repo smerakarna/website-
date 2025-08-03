470132dce84bf39fb26cfcf1aee5da9b749bca8c
Smera <smerakarna@hotmail.com>
2025-08-03 10:09:01 -0700

    newfiles

--- index.ts
+++ index.ts
@@ -2,22 +2,26 @@
 
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import path from 'path'
 // Load environment variables from .env file
 dotenv.config();
 
 const app: Application = express();
-const PORT: number = parseInt(process.env.PORT || '3000', 10);
+const PORT: number = parseInt(process.env.PORT || '4000', 10);
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Routes
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express with TypeScript!');
+  res.sendFile(path.resolve(__dirname,"./index.html"))
 });
-
+app.get('/register', (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname,'./register.html'));
+});
+app.post('/register', (req: Request, res: Response) => {
+  console.log(req.body)
+});
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ requirements.md
@@ -0,0 +1,18 @@
+# User Stories
+1. As a user, I should be able to register an account.
+2. As a user, I should be able to watch videos.
+3. As a website owner, I should be able to delete accounts.
+4. As a website owner, I should be able to post material
+5. As a website owner, I should be able to add owners/tutors
+6. As a website owner, I should be able to chat with users
+7. As a user I should be able to chat with the website owners(tutors)
+8. As a website owner, I should be able to add documents and files
+9. As a website user, I should be able see documents and files
+10. As a website user I should be able to login to my account
+11. As a website owner I should be able to delete accounts
+12. As a website user, I should be able to delete my account
+
+# Requirements
+1. Registration webpage [1]
+2. Registration form [1]
+3. Data storage method
